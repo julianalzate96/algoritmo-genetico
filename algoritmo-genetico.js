51e11af41cca962fbdb0c33593ee140321bd8fa9
julianalzate96 <julian.alzate@parrolabs.com>
2021-08-13 00:46:23 -0500

    Code formatted

--- algoritmo-genetico.js
+++ algoritmo-genetico.js
@@ -19,30 +19,30 @@ const DICTIONARY = [
   "z",
   " ",
   "í",
-]
+];
 
 /**
  * LA PROBABILIDAD DE MUTACION LA ESTAMOS UTILIZANDO PARA
  * QUE NOS DE UN VALOR DE 0 A 5 Y SEA LA CANTIDAD DE CARACTERES QUE VAN A MUTAR
  * EN EL ARREGLO
  */
-const PROBABILIDAD_MUTACION = Math.floor(Math.random() * 5)
+const PROBABILIDAD_MUTACION = Math.floor(Math.random() * 5);
 
 /**
  * SE DEFINE LA CADENA OBJETIVO Y SE TRANSFORMA
  * EN UN ARRAY DE CARACTERES
  * (LA CADENA OBJETIVO SE DEFINE EN MINUSCULA PARA ASI MINIMIZAR EL DICCIONARIO)
  * */
-const GOAL = "politecnico colombiano jaime isaza cadavid"
-let goalArray = GOAL.split("")
+const GOAL = "politecnico colombiano jaime isaza cadavid";
+let goalArray = GOAL.split("");
 
 /**
  * SE DEFINEN LAS VARIABLES NECESARIAS PARA EL ALGORITMO
  */
-let generation = 0 //GENERACION YA QUE EL ALGORITMO SE VA A BASAR EN GENERACION Y NO EN CONVERGENCIA
-let bestCromosomas = [] //ARREGLO CON LOS MEJORES CROMOSOMAS DE LA GENERACION
-let childs = [] // HIJOS RESULTANTES DE EL CRUZE Y LA MUTACION
-var poblation = [] // POBLACION
+let generation = 0; //GENERACION YA QUE EL ALGORITMO SE VA A BASAR EN GENERACION Y NO EN CONVERGENCIA
+let bestCromosomas = []; //ARREGLO CON LOS MEJORES CROMOSOMAS DE LA GENERACION
+let childs = []; // HIJOS RESULTANTES DE EL CRUZE Y LA MUTACION
+var poblation = []; // POBLACION
 
 /**
  * FUNCION FITNESS ESTA ESTA DEFINIDA PARA CONTAR CUANTOS CARACTERES ESYAN
@@ -53,16 +53,16 @@ var poblation = [] // POBLACION
  * FITNESS DE 39)
  */
 const getFitness = (cromosoma) => {
-  let fitness = 0
+  let fitness = 0;
 
   //SE RECORRE LA FUNCION OBJETIVO Y SE VALIDA EN CADA POSICION SY SON IGUALES
   for (let index = 0; index < goalArray.length; index++) {
     if (goalArray[index] === cromosoma[index]) {
-      fitness += 1
+      fitness += 1;
     }
   }
-  return fitness
-}
+  return fitness;
+};
 
 /**
  * FUNCION PARA GENERAR LA POBLACION LA CUAL RECIBE COMO PARAMETRO
@@ -71,52 +71,52 @@ const getFitness = (cromosoma) => {
  * UN ARREGLO DEL TAMAÑO DE LA CADENA OBJETIVO
  */
 const generatePoblation = (n) => {
-  let cromosomas = []
+  let cromosomas = [];
   for (let index = 0; index < n; index++) {
-    let cromosoma = []
+    let cromosoma = [];
     for (let index = 0; index < goalArray.length; index++) {
-      let random = Math.floor(Math.random() * DICTIONARY.length)
-      cromosoma[index] = DICTIONARY[random]
+      let random = Math.floor(Math.random() * DICTIONARY.length);
+      cromosoma[index] = DICTIONARY[random];
     }
-    cromosomas.push(cromosoma)
+    cromosomas.push(cromosoma);
   }
-  return cromosomas
-}
+  return cromosomas;
+};
 
 // FUNCION DE SELECCION Y CRUCE
 const selection_and_cruce = (firstTime) => {
-  let pointCruze = 0
-  let child1 = []
-  let child2 = []
+  let pointCruze = 0;
+  let child1 = [];
+  let child2 = [];
 
   // SE LIMPIA EL ARREGLO DE HIJOS
-  childs = []
+  childs = [];
 
   /**
    * ESTA VALIDACION ES SOLO PARA MOSTRAR LOS MEJORES CROMOSOMAS EN DEFINIDAS GENERACIONES
    */
   if (generation === 250 || generation === 500 || generation === 750) {
-    console.log("MEJORES CROMOSOMAS EN LA GENERACION: ", generation)
+    console.log("MEJORES CROMOSOMAS EN LA GENERACION: ", generation);
     bestCromosomas.forEach((cromosoma) => {
-      console.log("CROMOSOMA PADRE: ", cromosoma.cromosoma.join(""))
-      console.log("FITNESS: ", cromosoma.fitness)
-      console.log("-")
-    })
-    console.log("-----------------------")
+      console.log("CROMOSOMA PADRE: ", cromosoma.cromosoma.join(""));
+      console.log("FITNESS: ", cromosoma.fitness);
+      console.log("-");
+    });
+    console.log("-----------------------");
   }
 
   /**
    * SE RECORRE LA POBLACION Y SE SACAN LOS 2 MEJORES CROMOSOMAS
    */
   poblation.forEach((cromosoma) => {
-    let resultFitness = getFitness(cromosoma) //SE OBTIENE EL FITNESS DE EL CROMOSOMA
+    let resultFitness = getFitness(cromosoma); //SE OBTIENE EL FITNESS DE EL CROMOSOMA
 
     if (bestCromosomas.length > 1) {
       //SE VALIDA QUE EL ARRAY DE BESTCROMOSOMAS TENGA ALMENOS 2
       bestCromosomas.sort((a, b) => {
         //SE ORDENA DE MAYOR A MENOR PARA ASI DACILITAR LA VALIDACION
-        return a.fitness - b.fitness
-      })
+        return a.fitness - b.fitness;
+      });
 
       /**
        * SE VALIDA QUE EL FITNESS SEA MAYOR QUE EL QUE MENOS TIENE EN EL ARREGLO
@@ -126,19 +126,19 @@ const selection_and_cruce = (firstTime) => {
         resultFitness > bestCromosomas[0].fitness &&
         cromosoma !== bestCromosomas[1].cromosoma
       ) {
-        bestCromosomas[0] = { fitness: resultFitness, cromosoma }
+        bestCromosomas[0] = { fitness: resultFitness, cromosoma };
       }
     } else {
       //EN ESTA VALIDACION NOS ASEGURAMOS DE QUE LOS 2 PRIMEROS CROMOSOMAS NO SEAN IGUALES
       if (bestCromosomas.length) {
         if (bestCromosomas[0].cromosoma !== cromosoma) {
-          bestCromosomas.push({ cromosoma, fitness: resultFitness })
+          bestCromosomas.push({ cromosoma, fitness: resultFitness });
         }
       } else {
-        bestCromosomas.push({ cromosoma, fitness: resultFitness })
+        bestCromosomas.push({ cromosoma, fitness: resultFitness });
       }
     }
-  })
+  });
 
   /**
    * ESTA VALIDACION ERA PARA LIMPIAR LA POBLACION
@@ -155,27 +155,27 @@ const selection_and_cruce = (firstTime) => {
    * LA PROBABILIDAD DE CRUZE NOS VA A GENERAR UN VALOR ALEATORIO ENTRE
    * 0 Y EL TAMAÑO DE DE LA CADENA OBJETIVO PARA SI PARTIR EL ARREGLO
    */
-  pointCruze = Math.floor(Math.random() * goalArray.length)
+  pointCruze = Math.floor(Math.random() * goalArray.length);
 
   //SE PARTE EL PADRE DESDE EL PUNTO CRUCE HASTA EL FINAL Y SE LE ASIGNA AL HIJO
   child1 = bestCromosomas[1].cromosoma.slice(
     pointCruze,
     bestCromosomas[1].cromosoma.length
-  )
+  );
   // SE PARTE EL OTRO PADRE DEL PRINCIPIO HASTA EL PUNTO DE CRUCE Y SE CONCATENA CON ELE OTRO
-  child1 = [...bestCromosomas[0].cromosoma.slice(0, pointCruze), ...child1]
+  child1 = [...bestCromosomas[0].cromosoma.slice(0, pointCruze), ...child1];
 
   //IGUAL PARA GENERAR EL OTRO HIJO
   child2 = bestCromosomas[0].cromosoma.slice(
     pointCruze,
     bestCromosomas[0].cromosoma.length
-  )
-  child2 = [...bestCromosomas[1].cromosoma.slice(0, pointCruze), ...child2]
+  );
+  child2 = [...bestCromosomas[1].cromosoma.slice(0, pointCruze), ...child2];
 
   //SE AGREGAN LOS HIJOS AL ARREGLO DE HIJOS
-  childs.push(child1)
-  childs.push(child2)
-}
+  childs.push(child1);
+  childs.push(child2);
+};
 
 /**
  * FUNCION DE MUTACION LA CUAL DEPENDIENDO DE LA PROBABILIDAD DE MUTACION VA
@@ -185,53 +185,53 @@ const selection_and_cruce = (firstTime) => {
 const mutation = () => {
   childs.forEach((child) => {
     for (let i = 0; i < PROBABILIDAD_MUTACION; i++) {
-      let probMutation = Math.floor(Math.random() * goalArray.length) //POSICION QUE VA A CAMBIAR
-      let newValue = Math.floor(Math.random() * DICTIONARY.length) //VALOR EN EL DICCIONARIO QUE VAMOS A USAR
+      let probMutation = Math.floor(Math.random() * goalArray.length); //POSICION QUE VA A CAMBIAR
+      let newValue = Math.floor(Math.random() * DICTIONARY.length); //VALOR EN EL DICCIONARIO QUE VAMOS A USAR
 
-      child[probMutation] = DICTIONARY[newValue] //SE MUTA EL HIJO
+      child[probMutation] = DICTIONARY[newValue]; //SE MUTA EL HIJO
     }
-  })
+  });
 
   // VALIDACION SOLO PARA MOSTRAR LOS HIJOS EN CIERTAS GENERACIONES
   if (generation === 250 || generation === 500 || generation === 750) {
     console.log(
       "HIJOS DE LOS MEJORES CROMOSOMAS EN LA GENERACION: ",
       generation
-    )
+    );
     childs.forEach((child) => {
-      console.log("CROMOSOMA HIJO; ", child.join(""))
-      console.log("-")
-    })
-    console.log("-----------------------")
+      console.log("CROMOSOMA HIJO; ", child.join(""));
+      console.log("-");
+    });
+    console.log("-----------------------");
   }
 
   /**
    * SE AGREGAN LOS HIJOS A LA POBLACION PARA ASI SER VALIDADOS EN LA SELECCION
    */
 
-  poblation = [...childs, ...poblation]
-}
+  poblation = [...childs, ...poblation];
+};
 
 // GENERAMOS LA POBLACION INICIAL
-poblation = generatePoblation(10)
+poblation = generatePoblation(10);
 
-console.log("POBLACION INICIAL: ")
-console.log("-----------------------")
+console.log("POBLACION INICIAL: ");
+console.log("-----------------------");
 
 // MOSTRAMOS LA POBLACION
 poblation.forEach((cromosoma) => {
-  console.log(cromosoma.join(""))
-  console.log("-")
-})
-console.log("-----------------------")
+  console.log(cromosoma.join(""));
+  console.log("-");
+});
+console.log("-----------------------");
 
 //WHILE DE GENERACIONES
 while (generation < 2000) {
-  selection_and_cruce(generation === 1)
-  mutation()
-  generation += 1
+  selection_and_cruce(generation === 1);
+  mutation();
+  generation += 1;
 }
 
 //SE MUESTRA EL CROMOSOMA CON EL MEJOR FITNESS
-console.log("MEJOR CROMOSOMA: ", bestCromosomas[0].cromosoma.join(""))
-console.log("FITNESS: ", bestCromosomas[0].fitness)
+console.log("MEJOR CROMOSOMA: ", bestCromosomas[0].cromosoma.join(""));
+console.log("FITNESS: ", bestCromosomas[0].fitness);
